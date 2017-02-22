d476c96f226ed5c6c08a8cab3d635ef27ee212eb
Jagroop Singh <geek.jagroop@gmail.com>
2017-02-22 09:54:32 +0530

    Added index method in Webcontroller as default app route

--- routes/web.js
+++ routes/web.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const web = require('./../controllers/WebController')
 
 router.get('/', web.index)
+router.get('/users', web.users)
 
 module.exports = router
