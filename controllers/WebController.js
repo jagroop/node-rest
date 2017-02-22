1e72bb297f24606835a2deb985a725f82ce7db04
Jagroop Singh <geek.jagroop@gmail.com>
2017-02-22 14:52:53 +0530

    clearning things up

--- controllers/WebController.js
+++ controllers/WebController.js
@@ -1,31 +1,16 @@
 const db = require('./../bootstrap/mysql')
 
 class WebController{
-
-	/**
-	 * WebController Constructor
-	 */
+	
 	constructor() {
 
 	}
-
-	/**
-	 * Default route of the app
-	 * @param  {object} req Request
-	 * @param  {object} res Response
-	 * @return {json}     Json
-	 */
+	
 	index(req, res) {
 		const node_app = {app : "node-rest", developer : "Jagroop Singh"}
 		res.json(node_app)
 	}
-
-	/**
-	 * Get All Users from users table
-	 * @param  {object} req Request
-	 * @param  {object} res Response
-	 * @return {json}     Users list json format
-	 */
+	
 	users(req, res) {
 		db.query('SELECT * FROM users', function (error, results, fields) {
 		  if (error) throw error
